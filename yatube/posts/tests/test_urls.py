966b4de60b8e60d8f77ad0eb2ee223b327208b08
Elbrus Karibov <elb.kar@yandex.ru>
2022-10-03 13:45:59 +0300

    post_test_url

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -6,51 +6,57 @@ from ..models import Group, Post
 
 User = get_user_model()
 
-URLS_AND_TEMPLATES_FOR_ALL = {
-    'posts/group_list.html': '/group/test_slug/',
-    'posts/profile.html': '/profile/Tester2000/',
-    'posts/post_detail.html': 'posts/<int:post_id>/',
-    'posts/index.html': '/',
-}
-
-URLS_AND_TEMPLATES_FOR_NOT_AUTHOR = {
-    'posts/create_post.html': '/create/',
-}
-
-URLS_AND_TEMPLATES_FOR_AUTHOR = {
-    'posts/create_post.html': '/edit/',
-    'posts/create_post.html': '/posts/<int:post_id>/edit/',
-}
-
-URLS_UNEXISTING = ['/unexisting_url/', ]
-
-
 class PostURLTests(TestCase):
     @classmethod
     def setUpClass(cls):
+
         super().setUpClass()
-        cls.user = User.objects.create_user(username='auth')
-        Group.objects.create(
+        cls.user_author = User.objects.create_user(username='Tester2000')
+        cls.user = User.objects.create_user(username='simple_user')
+        cls.group = Group.objects.create(
             title='Тестовая группа',
             slug='test_slug',
             description='Тестовое описание',
         )
-        Post.objects.create(
+        cls.post = Post.objects.create(
             text='Тестовый текст',
-            author=cls.user,
+            author=cls.user_author,
+            group=cls.group,
         )
+
+        cls.URLS_AND_TEMPLATES_FOR_ALL = {
+            'posts/group_list.html': '/group/test_slug/',
+            'posts/profile.html': '/profile/Tester2000/',
+            'posts/post_detail.html': f'/posts/{cls.post.id}/',
+            'posts/index.html': '/',
+        }
+
+        cls.URLS_AND_TEMPLATES_FOR_NOT_AUTHOR = {
+            'posts/create_post.html': '/create/',
+        }
+
+        cls.URLS_AND_TEMPLATES_FOR_AUTHOR = {
+            'posts/create_post.html': '/edit/',
+            'posts/create_post.html': f'/posts/{cls.post.id}/edit/',
+        }
+
+        cls.URLS_UNEXISTING = ['/unexisting_url/', ]
+
+        cls.URLS_REDIRECT_FOR_GUEST = {
+            f'/posts/{cls.post.id}/edit/': f'/auth/login/?next=/posts/{cls.post.id}/edit/',
+            '/create/': '/auth/login/?next=/create/',
+        }
 
     def setUp(self):
         self.guest_client = Client()
-        self.user = User.objects.create_user(username='Tester2000')
         self.authorized_client = Client()
-        self.authorized_client.force_login(self.user)
+        self.authorized_client.force_login(self.user_author)
         self.authorized_client_but_not_author = Client()
-        self.authorized_client_but_not_author.force_login(PostURLTests.user)
+        self.authorized_client_but_not_author.force_login(self.user)
 
     def test_urls_and_templates_exists_at_desired_location_for_unauth(self):
         '''Проверка доступных страниц и шаблонов для неавторизованных пользователей.'''
-        for template, url in URLS_AND_TEMPLATES_FOR_ALL.items():
+        for template, url in self.URLS_AND_TEMPLATES_FOR_ALL.items():
             with self.subTest(url=url):
                 response_guest = self.guest_client.get(url)
                 self.assertEqual(response_guest.status_code, 200)
@@ -58,7 +64,7 @@ class PostURLTests(TestCase):
 
     def test_urls_unexists_at_desired_location_for_all_users(self):
         '''Проверка несуществующих страниц для всех пользователей.'''
-        for url in URLS_UNEXISTING:
+        for url in self.URLS_UNEXISTING:
             with self.subTest(url=url):
                 response_guest = self.guest_client.get(url)
                 self.assertEqual(response_guest.status_code, 404)
@@ -71,7 +77,7 @@ class PostURLTests(TestCase):
 
     def test_urls_and_templates_exists_at_desired_location_for_auth_not_author(self):
         '''Проверка страниц и шаблонов для авторизованных пользователей,не являющихся автором.'''
-        for template, url in URLS_AND_TEMPLATES_FOR_NOT_AUTHOR.items():
+        for template, url in self.URLS_AND_TEMPLATES_FOR_NOT_AUTHOR.items():
             with self.subTest(url=url):
                 response_not_author = self.authorized_client_but_not_author.get(url)
                 self.assertEqual(response_not_author.status_code, 200)
@@ -79,11 +85,18 @@ class PostURLTests(TestCase):
 
     def test_urls_and_templates_exists_at_desired_location_for_author(self):
         '''Проверка страниц и шаблонов для авторизованного пользователя,являющегося автором.'''
-        for template, url in URLS_AND_TEMPLATES_FOR_AUTHOR.items():
+        for template, url in self.URLS_AND_TEMPLATES_FOR_AUTHOR.items():
             with self.subTest(url=url):
                 response_author = self.authorized_client.get(url)
                 self.assertEqual(response_author.status_code, 200)
                 self.assertTemplateUsed(response_author, template)
 
-
-
+    def test_redirect_urls_for_unauthorized_user(self):
+        '''Проверка переадрисации на страницу авторизации
+        для гостевых пользователей с последующим редиректом.'''
+        for url, redirect_url in self.URLS_REDIRECT_FOR_GUEST.items():
+            with self.subTest(url=url):
+                response = self.guest_client.get(url, follow=True)
+                self.assertRedirects(
+                    response, redirect_url
+                )
